4b6235e91e9ce71e6b07c0b5d51ac45f73b8bde7
yosefsahalo <yosefsahalu@gmail.com>
2021-02-04 17:06:26 +0200

    new mission is done

--- main.js
+++ main.js
@@ -19,15 +19,18 @@
 
 
 // mission 2
-// function evevNumber(params) {
+// function evevNumber() {
 //     var num = 0;
+//     var num1 = [];
 //     var even = [];
 //     for (let i = 0; i < 10; i++) {
 //         num = +prompt('Enter Your Number');
+//         num1.push(num);
 //         if (num % 2 == 0) {
 //             even.push(num);
 //         }
 //     }
+//     console.log(num1);
 //     console.log(even);
 // }
 // evevNumber();
@@ -59,7 +62,7 @@
 // function names() {
 //     var name = prompt('Enter Your Name here');
 //     var counter = 0;
-//     var counter1 =0
+//     var counter1 =0;
 //     for (let i = 0; i < name.length; i++) {
 //         if (name[0] == 'A') {
 //             counter++;
@@ -68,7 +71,7 @@
 //             counter1++;
 //         }
 //         else{
-            
+
 //         }       
 //     }
 //     if (counter>0) {
@@ -84,3 +87,102 @@
 //     console.log(name);
 // }
 // names();
+
+
+// // miisםin 4 again
+// function names(leng) {
+//     var name = prompt('Enter Your Name here');
+//     // var counter = 0;
+//     // var counter1 = 0;
+//     for (let i = 0; i < leng.length; i++) {
+//         if (name[0] == leng[i]) {
+//             if (leng[i] == 'A') {
+//                 console.log('Nice');
+//             }
+//             else if (leng[i] == 'Z') {
+//                 console.log('Good');
+//             }
+//             else {
+
+//             }
+//         }
+//     }
+//     console.log('This Letter Is Not Important');
+// }
+// var abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// names(abc);
+
+
+// mission 5
+// function fizzBuzz() {
+//     for (let i = 1; i < 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log(i,'fizzbuzz');
+//         }
+//         if (i % 3 != 0 && i % 5 != 0) {
+//             console.log(i);
+//         }
+//         if (i % 3 == 0) {
+//             console.log(i,'fizz');
+//         }
+//         if (i % 5 == 0) {
+//             console.log(i,'buzz');
+//         }
+//     }
+// }
+// fizzBuzz();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function firstNameLastName() {
+//     var firsName= prompt('Enter Your First Name');
+//     var lastName= prompt('Enter Your First Name');
+//     var result = firsName.indexoff(0);
+//     var result1 = lastName.toUppercase();
+//     var sum = lastName + listName;
+// }
+// firstNameLastName();
+
+
+// function printName() {
+//     var inputFromUser = prompt("Enter Your Name");
+//     if (inputFromUser / 1 == inputFromUser) {
+//         alert("Please Enter a Proper Name And Not a Number")
+//     }
+// }
